2182b8c39909ce9561fd6f46f8e840688c9356d2
tatiana_saturno <tatianassaturno@gmail.com>
2016-08-20 15:03:53 -0300

    modificando leitura para utf-8

--- u_gestoraXconvenios.R
+++ u_gestoraXconvenios.R
@@ -2,8 +2,8 @@
 #mudar para o seu
 setwd("C:/Users/Tatiana/Downloads/Compressed/hackfest/tarefa1- unidade_gestora x convenio") 
 
-convenios <- read.csv("contratos.csv")
-u_gestora <- read.csv("codigo_ugestora.csv")
+convenios <- read.csv("contratos.csv", encoding="UTF-8")
+u_gestora <- read.csv("codigo_ugestora.csv", encoding="UTF-8")
 
 #pegar convenios pela unidade gestora
 convenios_u_gestora <- subset(convenios, cd_UGestora == "101025")
