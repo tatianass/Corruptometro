ecc1899b93f1f97fe2fb977a69609afc47d433c9
tatiana_saturno <tatianassaturno@gmail.com>
2016-08-21 04:44:01 -0300

    emotions

--- site/angular/controller/indexController.js
+++ site/angular/controller/indexController.js
@@ -1,5 +1,29 @@
 app.controller("IndexController", ["$scope", function($scope){
-	$scope.value = 0
+    $scope.value = 0;
+    $scope.emotion = "emotion/1.png";
+    var emotions = [
+        {p: 0.2, src:"emotion/1.png"},
+        {p: 0.4, src:"emotion/2.png"},
+        {p: 0.6, src:"emotion/3.png"},
+        {p: 0.8, src:"emotion/4.png"},
+        {p: 1.0, src:"emotion/5.png"}];
+
+    var updateEmotion = function(p){
+        if(p <= emotions[0].p){
+            $scope.emotion = emotions[0].src
+        } else if(p > emotions[0].p & p <= emotions[1].p){
+            $scope.emotion = emotions[1].src
+        } else if (p > emotions[1].p & p <= emotions[2].p){
+            $scope.emotion = emotions[2].src
+        } else if(p > emotions[2].p & p <= emotions[3].p){
+            $scope.emotion = emotions[3].src
+        }  else{
+            $scope.emotion = emotions[4].src
+        }        
+    }
+
+
+    
     $scope.termometro = {
         chart: {
                 subcaptionFontBold: "0",
@@ -18,16 +42,16 @@ app.controller("IndexController", ["$scope", function($scope){
     }
 
     $scope.search = function(){
-         $scope.termometro.value = $scope.value;
-         
+        $scope.termometro.value = $scope.value;
         
-
-color = getColorForPercentage($scope.value/100)
-console.log(color);
-         $scope.termometro.chart.thmFillColor = getColorForPercentage($scope.value/100);
+        //pegar a cor segundo a porcentagem (>p mais vermelho) 
+        color = getColorForPercentage($scope.value/100);
+        color = colorToHex(color);
+        $scope.termometro.chart.thmFillColor = color;
+        updateEmotion($scope.value/100);
     }
 
-	$scope.features = {
+    $scope.features = {
         chart: {
             caption: "Índices de Corrupção",
             paletteColors: "#0075c2",
@@ -49,9 +73,12 @@ console.log(color);
     };
 
     var percentColors = [
-    { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
-    { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
-    { pct: 1.0, color: { r: 0x00, g: 0xff, b: 0 } } ];
+    { pct: 0.0, color: { r: 0x00, g: 0xff, b: 0x00 } },
+    { pct: 0.2, color: { r: 0xe6, g: 0xff, b: 0x00 } },
+    { pct: 0.4, color: { r: 0xff, g: 0xc4, b: 0x00 } },
+    { pct: 0.6, color: { r: 0xff, g: 0x9e, b: 0x00 } },
+    { pct: 0.8, color: { r: 0xff, g: 0x44, b: 0x00 } },
+    { pct: 1.0, color: { r: 0xff, g: 0x00, b: 0x00} } ];
 
     var getColorForPercentage = function(pct) {
         for (var i = 1; i < percentColors.length - 1; i++) {
@@ -65,6 +92,7 @@ console.log(color);
         var rangePct = (pct - lower.pct) / range;
         var pctLower = 1 - rangePct;
         var pctUpper = rangePct;
+        
         var color = {
             r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
             g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
@@ -75,17 +103,18 @@ console.log(color);
         // or output as hex if preferred
     }  
 
-    function colorToHex(color) {
-        if (color.substr(0, 1) === '#') {
-            return color;
-        }
-        var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+    var colorToHex = function (color) {
+
+        color[0] = parser(color[0]);
+        color[1] = parser(color[1]);
+        color[2] = parser(color[2]);
         
-        var red = parseInt(digits[2]);
-        var green = parseInt(digits[3]);
-        var blue = parseInt(digits[4]);
-        
-        var rgb = blue | (green << 8) | (red << 16);
-        return digits[1] + '#' + rgb.toString(16);
+        color = "#" + color.join("");
+        return color;        
     };
+
+    var parser = function(x){             //For each array element
+        x  = parseInt(x).toString(16);      //Convert to a base16 string
+        return (x.length==1) ? "0"+x : x;  //Add zero if we get only one character
+    }
 }])
