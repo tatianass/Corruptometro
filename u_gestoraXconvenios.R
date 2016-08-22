0a29d45817c57f00f3ded2c0d3bcb0b21529bb88
tatiana_saturno <tatianassaturno@gmail.com>
2016-08-22 17:22:06 -0300

    tirando o setwd

--- u_gestoraXconvenios.R
+++ u_gestoraXconvenios.R
@@ -1,6 +1,4 @@
 #Tatiana - tarefa 1 - especificacao: pegar a quantidade total de convenios por unidade gestora
-#mudar para o seu
-setwd("C:/Users/Tatiana/Downloads/Compressed/hackfest/tarefa1- unidade_gestora x convenio") 
 
 convenios <- read.csv("contratos.csv", encoding="UTF-8")
 u_gestora <- read.csv("codigo_ugestora.csv", encoding="UTF-8")
