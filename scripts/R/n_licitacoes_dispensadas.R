570fb14ed7d749652fef8cb2397b4b62c98b6b12
Jefferson <jefferson.ecs@gmail.com>
2016-08-30 01:28:27 -0300

    Correção do endereço do diretório data

--- scripts/R/n_licitacoes_dispensadas.R
+++ scripts/R/n_licitacoes_dispensadas.R
@@ -1,20 +1,24 @@
+####
+# Esse script foi adicionado no script dados.R
+####
+
 relabel_ano <- function(x){
   ifelse(x >= 2009 && x <2013, 2009, 2013)
 }
 
-tre_sagres_jul <- read.csv('TRE_Sagres_Resp_Eleito.csv', encoding = "UTF-8")
-tre_sagres_n_jul <- read.csv('TRE_Sagres_Eleit_Idon.csv', encoding = "UTF-8")
+tre_sagres_jul <- read.csv('../../data/TRE_Sagres_Resp_Eleito.csv', encoding = "UTF-8")
+tre_sagres_n_jul <- read.csv('../../data/TRE_Sagres_Eleit_Idon.csv', encoding = "UTF-8")
 
 tre_sagres_jul$class <- "Julgados"
-tre_sagres_n_jul$class <- "Não julgados"
+tre_sagres_n_jul$class <- "N?o julgados"
 
 
-tre_sagres_jul <- select(tre_sagres_jul,-DECISÃO, -RES..DECISÃO.PODER.LEGISLATIVO, -ITEM, -PROCESSO, -SUBCATEGORIA, -RESPONSÃVEL, -CPF)
+tre_sagres_jul <- select(tre_sagres_jul,-DECIS?O, -RES..DECIS?O.PODER.LEGISLATIVO, -ITEM, -PROCESSO, -SUBCATEGORIA, -RESPONS?VEL, -CPF)
 tre_sagres <- rbind(tre_sagres_jul, tre_sagres_n_jul)
 
-ugestora <- read.csv('codigo_ugestora.csv', encoding = "UTF-8")
-contrato <- read.csv('contratos.csv', encoding = "UTF-8")
-#tipo_modalidade_licitacao <- read.csv('tipo_modalidade_licitacao.csv', encoding = "UTF-8")
+ugestora <- read.csv('../../data/codigo_ugestora.csv', encoding = "UTF-8")
+contrato <- read.csv('../../data/contratos.csv', encoding = "UTF-8")
+#tipo_modalidade_licitacao <- read.csv('../../data/tipo_modalidade_licitacao.csv', encoding = "UTF-8")
 
 licitacoes <- subset(contrato, tp_Licitacao %in% c(6, 7) & dt_Ano > 2008)
 licitacoes$dt_Ano <- with(licitacoes, unlist(lapply(dt_Ano, relabel_ano)))
@@ -25,4 +29,4 @@ colnames(n.dispensas)[3] <- "n.dispensas"
 tre_sagres <- merge(tre_sagres, n.dispensas, all.x = T, by.x=c("cd_Ugestora","dt_Ano"), by.y = c("cd_UGestora","dt_Ano"))
 tre_sagres$n.dispensas <- with(tre_sagres, ifelse(is.na(n.dispensas),0,n.dispensas))
 
-write.table(tre_sagres, "tre_sagres_unificadoBase.csv", sep=";", row.names = F, quote = F, fileEncoding = "UTF-8")
+write.table(tre_sagres, "../../data/tre_sagres_unificadoBase.csv", sep=";", row.names = F, quote = F, fileEncoding = "UTF-8")
--- scripts/R/u_gestoraXconvenios.R
+++ scripts/R/u_gestoraXconvenios.R
@@ -1,7 +1,7 @@
 #Tatiana - tarefa 1 - especificacao: pegar a quantidade total de convenios por unidade gestora
 
-convenios <- read.csv("contratos.csv", encoding="UTF-8")
-u_gestora <- read.csv("codigo_ugestora.csv", encoding="UTF-8")
+convenios <- read.csv("../../data/contratos.csv", encoding="UTF-8")
+u_gestora <- read.csv("../../data/codigo_ugestora.csv", encoding="UTF-8")
 
 #pegar convenios pela unidade gestora
 convenios_u_gestora <- subset(convenios, cd_UGestora == "101025")
