b0236f6feb8ec9caec62f53a42bfaa79b56a8df2
Tatiana Silva <tatiana.silva@ccc.ufcg.edu.br>
2016-09-06 10:39:38 -0300

    colocando novas features

--- site/angular/controller/indexController.js
+++ site/angular/controller/indexController.js
@@ -6,13 +6,10 @@ app.controller("IndexController", ["$scope", "$http", function($scope, $http){
     $scope.featureData = {}
     var mediaFeature = [
                         {
-                            "value": "50"
+                            "value": "38"
                         }, 
                         {
-                            "value": "50"
-                        }, 
-                        {
-                            "value": "50"
+                            "value": "61"
                         }
                     ]
 
@@ -86,16 +83,12 @@ app.controller("IndexController", ["$scope", "$http", function($scope, $http){
 
         //atualizando os valores dos indicadores
         $scope.featureData=[{
-            label: "QAL",
-            value: $scope.info.QAL
+            label: "NAV",
+            value: $scope.info.NAV
         },
         {
-            label: "QDL",
-            value: $scope.info.QDL
-        },
-        {
-            label: "QCL",
-            value: $scope.info.QCL
+            label: "NAT",
+            value: $scope.info.NAV
         }]
 
         $scope.features.dataset[0].data = $scope.featureData;
@@ -129,13 +122,10 @@ app.controller("IndexController", ["$scope", "$http", function($scope, $http){
                 {
                     "category": [
                         {
-                            "label": "Aditivos em Licitações"
+                            "label": "Aditivos de Valor"
                         }, 
                         {
-                            "label": "Dispensas de Licitações"
-                        }, 
-                        {
-                            "label": "Convites de Licitações"
+                            "label": "Aditivos Totais"
                         }
                     ]
                 }
@@ -203,190 +193,5 @@ app.controller("IndexController", ["$scope", "$http", function($scope, $http){
     }
 
     //dados
-    $scope.dados = [
-      {
-        "Nome": "TARCISIO ALVES FIRMINO",
-        "Prefeitura": "ÁGUA BRANCA",
-        "QCL": 24,
-        "QDL": 16,
-        "QAL": 66,
-        "Probabilidade": 0.418
-      },
-      {
-        "Nome": "EXPEDITO PEREIRA DE SOUZA",
-        "Prefeitura": "BAYEUX",
-        "QCL": 54,
-        "QDL": 78,
-        "QAL": 135,
-        "Probabilidade": 0.466
-      },
-      {
-        "Nome": "JOSIVAL JUNIOR DE SOUZA",
-        "Prefeitura": "BAYEUX",
-        "QCL": 237,
-        "QDL": 84,
-        "QAL": 179,
-        "Probabilidade": 0.726
-      },
-      {
-        "Nome": "ARLINDO FRANCISCO DE SOUSA",
-        "Prefeitura": "CACHOEIRA DOS ÍNDIOS",
-        "QCL": 0,
-        "QDL": 0,
-        "QAL": 0,
-        "Probabilidade": 0.456
-      },
-      {
-        "Nome": "GERALDO TERTO DA SILVA",
-        "Prefeitura": "CACIMBAS",
-        "QCL": 23,
-        "QDL": 23,
-        "QAL": 17,
-        "Probabilidade": 0.24
-      },
-      {
-        "Nome": "NILTON DE ALMEIDA",
-        "Prefeitura": "CACIMBAS",
-        "QCL": 246,
-        "QDL": 36,
-        "QAL": 4,
-        "Probabilidade": 0.58
-      },
-      {
-        "Nome": "EDUARDO RONIELLE GUIMARAES MARTINS DANTAS",
-        "Prefeitura": "CUBATI",
-        "QCL": 14,
-        "QDL": 4,
-        "QAL": 20,
-        "Probabilidade": 0.346
-      },
-      {
-        "Nome": "FERNANDA MARIA MARINHO DE MEDEIROS LOUREIRO",
-        "Prefeitura": "EMAS",
-        "QCL": 122,
-        "QDL": 11,
-        "QAL": 5,
-        "Probabilidade": 0.66
-      },
-      {
-        "Nome": "GILBERTO MUNIZ DANTAS",
-        "Prefeitura": "FAGUNDES",
-        "QCL": 305,
-        "QDL": 7,
-        "QAL": 1,
-        "Probabilidade": 0.78
-      },
-      {
-        "Nome": "FRANCIVALDO SANTOS DE ARAUJO",
-        "Prefeitura": "FREI MARTINHO",
-        "QCL": 85,
-        "QDL": 0,
-        "QAL": 13,
-        "Probabilidade": 0.122
-      },
-      {
-        "Nome": "AUSTERLIANO EVALDO ARAUJO",
-        "Prefeitura": "GADO BRAVO",
-        "QCL": 36,
-        "QDL": 8,
-        "QAL": 19,
-        "Probabilidade": 0.096
-      },
-      {
-        "Nome": "AUDIBERG ALVES DE CARVALHO",
-        "Prefeitura": "ITAPORANGA",
-        "QCL": 37,
-        "QDL": 65,
-        "QAL": 51,
-        "Probabilidade": 0.48
-      },
-      {
-        "Nome": "PAULO DALIA TEIXEIRA",
-        "Prefeitura": "JURIPIRANGA",
-        "QCL": 53,
-        "QDL": 84,
-        "QAL": 30,
-        "Probabilidade": 0.302
-      },
-      {
-        "Nome": "SUELI MADRUGA FREIRE",
-        "Prefeitura": "LAGOA DE DENTRO",
-        "QCL": 101,
-        "QDL": 8,
-        "QAL": 13,
-        "Probabilidade": 0.446
-      },
-      {
-        "Nome": "FABIANO PEDRO DA SILVA",
-        "Prefeitura": "LAGOA DE DENTRO",
-        "QCL": 29,
-        "QDL": 47,
-        "QAL": 61,
-        "Probabilidade": 0.302
-      },
-      {
-        "Nome": "OLIMPIO DE ALENCAR ARAUJO BEZERRA",
-        "Prefeitura": "MATARACA",
-        "QCL": 46,
-        "QDL": 4,
-        "QAL": 59,
-        "Probabilidade": 0.354
-      },
-      {
-        "Nome": "FRANCISCO ASSIS BRAGA JUNIOR",
-        "Prefeitura": "NAZAREZINHO",
-        "QCL": 212,
-        "QDL": 22,
-        "QAL": 27,
-        "Probabilidade": 0.686
-      },
-      {
-        "Nome": "MARIA DO CARMO SILVA",
-        "Prefeitura": "NOVA OLINDA",
-        "QCL": 78,
-        "QDL": 5,
-        "QAL": 2,
-        "Probabilidade": 0.84
-      },
-      {
-        "Nome": "JOSE DE ANCHIETA NOIA",
-        "Prefeitura": "PEDRA BRANCA",
-        "QCL": 18,
-        "QDL": 9,
-        "QAL": 51,
-        "Probabilidade": 0.302
-      },
-      {
-        "Nome": "RINALDO DE LUCENA GUEDES",
-        "Prefeitura": "PIRPIRITUBA",
-        "QCL": 212,
-        "QDL": 0,
-        "QAL": 9,
-        "Probabilidade": 0.126
-      },
-      {
-        "Nome": "ADAILMA FERNANDES DA SILVA",
-        "Prefeitura": "SERRA DA RAIZ",
-        "QCL": 26,
-        "QDL": 8,
-        "QAL": 33,
-        "Probabilidade": 0.27
-      },
-      {
-        "Nome": "MANOEL MARCELO DE ANDRADE",
-        "Prefeitura": "SERRA REDONDA",
-        "QCL": 65,
-        "QDL": 0,
-        "QAL": 7,
-        "Probabilidade": 0.044
-      },
-      {
-        "Nome": "EDMILSON ALVES DOS REIS",
-        "Prefeitura": "TEIXEIRA",
-        "QCL": 29,
-        "QDL": 64,
-        "QAL": 70,
-        "Probabilidade": 0.374
-      }
-    ]
+    $scope.dados = [{"Nome":"JOSE ADEMAR DE FARIAS","Prefeitura":"ALCANTIL","NAV":20,"NAT":40,"Probabilidade":0.178},{"Nome":"RENATO MENDES LEITE","Prefeitura":"ALHANDRA","NAV":108,"NAT":178,"Probabilidade":0.06},{"Nome":"MARCELO RODRIGUES DA COSTA","Prefeitura":"ALHANDRA","NAV":75,"NAT":130,"Probabilidade":0.568},{"Nome":"JOSE ARNALDO DA SILVA","Prefeitura":"AMPARO","NAV":16,"NAT":22,"Probabilidade":0.216},{"Nome":"PAULO GOMES PEREIRA","Prefeitura":"AREIA","NAV":7,"NAT":11,"Probabilidade":0.072},{"Nome":"CICERO PEDRO MEDA DE ALMEIDA","Prefeitura":"AREIAL","NAV":35,"NAT":53,"Probabilidade":0.38},{"Nome":"MYLTON DOMINGUES DE AGUIAR MARQUES","Prefeitura":"AROEIRAS","NAV":45,"NAT":81,"Probabilidade":0.632},{"Nome":"DOUGLAS LUCENA MOURA DE MEDEIROS","Prefeitura":"BANANEIRAS","NAV":46,"NAT":86,"Probabilidade":0.496},{"Nome":"MANOEL ALMEIDA DE ANDRADE","Prefeitura":"BARRA DE SANTANA","NAV":14,"NAT":20,"Probabilidade":0.26},{"Nome":"EXPEDITO PEREIRA DE SOUZA","Prefeitura":"BAYEUX","NAV":134,"NAT":208,"Probabilidade":0.082},{"Nome":"GERVAZIO GOMES DOS SANTOS","Prefeitura":"BERNARDINO BATISTA","NAV":48,"NAT":84,"Probabilidade":0.218},{"Nome":"MARIA LEONICE LOPES VITAL","Prefeitura":"BOA VENTURA","NAV":22,"NAT":44,"Probabilidade":0.004},{"Nome":"ROBERTO BANDEIRA DE MELO BARBOSA","Prefeitura":"BOM JESUS","NAV":4,"NAT":8,"Probabilidade":0.332},{"Nome":"MARIA PAULA GOMES PEREIRA","Prefeitura":"BORBOREMA","NAV":30,"NAT":52,"Probabilidade":0.14},{"Nome":"FRANCISCO DUTRA SOBRINHO","Prefeitura":"BREJO DO CRUZ","NAV":68,"NAT":112,"Probabilidade":0.138},{"Nome":"LAURI FERREIRA DA COSTA","Prefeitura":"BREJO DOS SANTOS","NAV":42,"NAT":55,"Probabilidade":0.29},{"Nome":"LUIZ VIEIRA DE ALMEIDA","Prefeitura":"BREJO DOS SANTOS","NAV":59,"NAT":99,"Probabilidade":0.512},{"Nome":"ORISMAN FERREIRA DA NOBREGA","Prefeitura":"CACIMBA DE AREIA","NAV":11,"NAT":22,"Probabilidade":0.164},{"Nome":"NILTON DE ALMEIDA","Prefeitura":"CACIMBAS","NAV":4,"NAT":6,"Probabilidade":0.92},{"Nome":"GERALDO TERTO DA SILVA","Prefeitura":"CACIMBAS","NAV":17,"NAT":32,"Probabilidade":0.076},{"Nome":"FRANCISCA DENISE ALBUQUERQUE DE OLIVEIRA","Prefeitura":"CAJAZEIRAS","NAV":100,"NAT":167,"Probabilidade":0.084},{"Nome":"ROMERO RODRIGUES VEIGA","Prefeitura":"CAMPINA GRANDE","NAV":325,"NAT":588,"Probabilidade":0.214},{"Nome":"VENEZIANO VITAL DO REGO SEGUNDO NETO","Prefeitura":"CAMPINA GRANDE","NAV":115,"NAT":170,"Probabilidade":0.064},{"Nome":"EDVALDO CARLOS FREIRE JUNIOR","Prefeitura":"CAPIM","NAV":22,"NAT":32,"Probabilidade":0.174},{"Nome":"JOSE ARDISON PEREIRA","Prefeitura":"CARRAPATEIRA","NAV":0,"NAT":0,"Probabilidade":0.79},{"Nome":"CAIO RODRIGO BEZERRA PAIXAO","Prefeitura":"CONDADO","NAV":25,"NAT":45,"Probabilidade":0.018},{"Nome":"EDILSON PEREIRA DE OLIVEIRA","Prefeitura":"COREMAS","NAV":111,"NAT":170,"Probabilidade":0.104},{"Nome":"ANTONIO CARLOS CAVALCANTI LOPES","Prefeitura":"COREMAS","NAV":73,"NAT":105,"Probabilidade":0.658},{"Nome":"GIVALDO LIMEIRA DE FARIAS","Prefeitura":"COXIXOLA","NAV":32,"NAT":53,"Probabilidade":0.348},{"Nome":"GUILHERME CUNHA MADRUGA JUNIOR","Prefeitura":"CUITEGI","NAV":27,"NAT":41,"Probabilidade":0.582},{"Nome":"JOAQUIM ALVES BARBOSA FILHO","Prefeitura":"CURRAL VELHO","NAV":1,"NAT":3,"Probabilidade":0.834},{"Nome":"DILSON DE ALMEIDA","Prefeitura":"DESTERRO","NAV":0,"NAT":0,"Probabilidade":0.79},{"Nome":"EDSON GOMES DE LUNA","Prefeitura":"DUAS ESTRADAS","NAV":38,"NAT":69,"Probabilidade":0.382},{"Nome":"AGUIFAILDO LIRA DANTAS","Prefeitura":"FREI MARTINHO","NAV":70,"NAT":140,"Probabilidade":0.262},{"Nome":"MARIA DE FATIMA DE AQUINO PAULINO","Prefeitura":"GUARABIRA","NAV":456,"NAT":648,"Probabilidade":0.492},{"Nome":"ZENOBIO TOSCANO DE OLIVEIRA","Prefeitura":"GUARABIRA","NAV":116,"NAT":181,"Probabilidade":0.026},{"Nome":"ALDO LUSTOSA DA SILVA","Prefeitura":"IMACULADA","NAV":19,"NAT":38,"Probabilidade":0.054},{"Nome":"ANTONIO CARLOS RODRIGUES DE MELO JUNIOR","Prefeitura":"ITABAIANA","NAV":1,"NAT":3,"Probabilidade":0.834},{"Nome":"ARON RENE MARTINS DE ANDRADE","Prefeitura":"ITATUBA","NAV":42,"NAT":73,"Probabilidade":0.42},{"Nome":"ANTONIO MAROJA GUEDES FILHO","Prefeitura":"JURIPIRANGA","NAV":8,"NAT":9,"Probabilidade":0.56},{"Nome":"PAULO DALIA TEIXEIRA","Prefeitura":"JURIPIRANGA","NAV":30,"NAT":52,"Probabilidade":0.14},{"Nome":"FABIANO PEDRO DA SILVA","Prefeitura":"LAGOA DE DENTRO","NAV":60,"NAT":107,"Probabilidade":0.168},{"Nome":"WILMESON EMMANUEL MENDES SARMENTO","Prefeitura":"LASTRO","NAV":8,"NAT":15,"Probabilidade":0.092},{"Nome":"MANOEL BENEDITO DE LUCENA FILHO","Prefeitura":"MALTA","NAV":44,"NAT":87,"Probabilidade":0.59},{"Nome":"MARCOS AURELIO MARTINS DE PAIVA","Prefeitura":"MARI","NAV":39,"NAT":65,"Probabilidade":0.836},{"Nome":"ANTONIO GOMES DA SILVA","Prefeitura":"MARI","NAV":22,"NAT":26,"Probabilidade":0.694},{"Nome":"PAULO FRACINETTE DE OLIVEIRA","Prefeitura":"MASSARANDUBA","NAV":0,"NAT":0,"Probabilidade":0.79},{"Nome":"OLIMPIO DE ALENCAR ARAUJO BEZERRA","Prefeitura":"MATARACA","NAV":58,"NAT":106,"Probabilidade":0.696},{"Nome":"MARIA DE FATIMA SILVA","Prefeitura":"MATINHAS","NAV":7,"NAT":12,"Probabilidade":0.07},{"Nome":"JAIRO HERCULANO DE MELO","Prefeitura":"MONTADAS","NAV":15,"NAT":29,"Probabilidade":0.088},{"Nome":"CLAUDIA APARECIDA DIAS","Prefeitura":"MONTE HOREBE","NAV":26,"NAT":30,"Probabilidade":0.192},{"Nome":"SALVAN MENDES PEDROZA","Prefeitura":"NAZAREZINHO","NAV":53,"NAT":84,"Probabilidade":0.204},{"Nome":"JOSE FELIX DE LIMA FILHO","Prefeitura":"NOVA PALMEIRA","NAV":3,"NAT":4,"Probabilidade":0.764},{"Nome":"GRIGORIO DE ALMEIDA SOUTO","Prefeitura":"OLIVEDOS","NAV":8,"NAT":15,"Probabilidade":0.092},{"Nome":"NATALIA CARNEIRO NUNES DE LIRA","Prefeitura":"OURO VELHO","NAV":40,"NAT":56,"Probabilidade":0.39},{"Nome":"MAGNO SILVA MARTINS","Prefeitura":"PASSAGEM","NAV":17,"NAT":34,"Probabilidade":0.6},{"Nome":"JOSE ANTONIO VASCONCELOS DA COSTA","Prefeitura":"PEDRA LAVRADA","NAV":0,"NAT":0,"Probabilidade":0.79},{"Nome":"DERIVALDO ROMAO DOS SANTOS","Prefeitura":"PEDRAS DE FOGO","NAV":55,"NAT":92,"Probabilidade":0.03},{"Nome":"CLAUDIO CHAVES COSTA","Prefeitura":"POCINHOS","NAV":15,"NAT":30,"Probabilidade":0.476},{"Nome":"JOAQUIM HUGO VIEIRA CARNEIRO","Prefeitura":"RIACHO DOS CAVALOS","NAV":53,"NAT":96,"Probabilidade":0.186},{"Nome":"EMMANUEL FELIPE LUCENA MESSIAS","Prefeitura":"SANTA HELENA","NAV":9,"NAT":15,"Probabilidade":0.066},{"Nome":"JAIRO HALLEY DE MOURA CRUZ","Prefeitura":"SERRA GRANDE","NAV":65,"NAT":106,"Probabilidade":0.114},{"Nome":"FLAVIO AURELIANO DA SILVA NETO","Prefeitura":"SOLEDADE","NAV":33,"NAT":60,"Probabilidade":0.242},{"Nome":"ANDRE AVELINO DE PAIVA GADELHA NETO","Prefeitura":"SOUSA","NAV":65,"NAT":109,"Probabilidade":0.088},{"Nome":"AILTON NIXON SUASSUNA PORTO","Prefeitura":"TAVARES","NAV":67,"NAT":102,"Probabilidade":0.25},{"Nome":"EDMILSON ALVES DOS REIS","Prefeitura":"TEIXEIRA","NAV":70,"NAT":118,"Probabilidade":0.148},{"Nome":"WENCESLAU SOUZA MARQUES","Prefeitura":"TEIXEIRA","NAV":34,"NAT":65,"Probabilidade":0.924}]
 }])
