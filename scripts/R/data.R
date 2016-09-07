033a4b0eb97779a7561bc0668b5974893e040602
allansales <allan.sales.89@gmail.com>
2016-09-07 15:46:30 -0300

    adição de cidades que estavam faltando e correção dos nomes das cidades em quantidadeEleitores.csv

--- scripts/R/data.R
+++ scripts/R/data.R
@@ -1,8 +1,8 @@
 source("imports.R")
 
-# Fun??o para modificar o ano.
-# Gest?es entre 2009 e 2012 tem o ano modificado para 2009
-# Gest?es entre 2013 e 2016 tem o ano modificado para 2013
+# Funcao para modificar o ano.
+# Gestoes entre 2009 e 2012 tem o ano modificado para 2009
+# Gestoes entre 2013 e 2016 tem o ano modificado para 2013
 relabel_ano <- function(x){
   ifelse(x >= 2009 && x <2013, 2009, 2013)
 }
@@ -10,27 +10,35 @@ relabel_ano <- function(x){
 # Carregar conjunto de dados
 tre_sagres <- read.csv("../../data/TRE_Sagres_Resp_Eleito_Final.csv",header=T, encoding = "UTF-8")
 ugestora <- read.csv('../../data/codigo_ugestora.csv', encoding = "UTF-8")
-contrato <- read.csv('../../data/contratos.csv', encoding = "UTF-8")
-aditivos <- read.csv("../../data/aditivos.csv", encoding = "UTF-8")
-quantidadeEleitores = read.csv("../../data/quantidadeEleitores.csv", encoding = "UTF-8")
-candidadosEleicao2016 = read.csv("../../data/Candidatos_eleicao_2016.csv", encoding = "UTF-8")
+quantidadeEleitores <- read.csv("../../data/quantidadeEleitores.csv", encoding = "UTF-8")
+candidadosEleicao2016 <- read.csv("../../data/Candidatos_eleicao_2016.csv", encoding = "UTF-8")
 candidadosEleicao2016$Candidato2016 <- TRUE
+
+aditivos <- read.csv("../../data/aditivos.csv", encoding = "UTF-8")
+aditivos$dt_Ano <- with(aditivos, unlist(lapply(dt_Ano, relabel_ano)))
+
+contrato <- read.csv('../../data/contratos.csv', encoding = "UTF-8")
+contrato <- subset(contrato, dt_Ano > 2008)
+contrato$dt_Ano <- with(contrato, unlist(lapply(dt_Ano, relabel_ano)))
 
 # Adiciona coluna com os candidatos a eleição em 2016
 tre_sagres <- merge(tre_sagres, candidadosEleicao2016, by.x = c("de_Ugestora","Eleito"), by.y = c("de_Ugestora","ELEITO"), all.x = T)
 tre_sagres[is.na(tre_sagres)] <- FALSE
 
+# Adiciona Quantidade de Eleitores por Municipio e Distancia da capital
+quantidadeEleitores <- select(quantidadeEleitores, Abrangencia, DistanciaParaCapital, Media_Eleitores = (Quantidade2009 + Quantidade2013)/2)
+tre_sagres <- merge(tre_sagres, quantidadeEleitores, by.x = c("de_Ugestora"), by.y = c("Abrangencia"), all.x = T)
+
+# Conta o numero de contratos da prefeitura
+nu_Contratos <- aggregate(nu_Contrato ~ cd_UGestora + dt_Ano, contrato, length) %>% rename(nu_Contratos = nu_Contrato)
+tre_sagres <- merge(tre_sagres, nu_Contratos, all.x = T, by.x=c("cd_Ugestora","dt_Ano"), by.y = c("cd_UGestora","dt_Ano"))
+
 # Adiciona a coluna "nu_Dispesas" a base
-# seleciona conjunto de contrados realizados apÃ³s o ano de 2008 com licitaÃ§Ãµes do tipo "Dispensa de valor" ou "Dispensa por outro motivo"
-licitacoes <- subset(contrato, tp_Licitacao %in% c(6, 7) & dt_Ano > 2008)
-licitacoes$dt_Ano <- with(licitacoes, unlist(lapply(dt_Ano, relabel_ano)))
-nu_Dispensas <- aggregate(tp_Licitacao ~ cd_UGestora + dt_Ano, licitacoes, length)
-colnames(nu_Dispensas)[3] <- "nu_Dispensas"
+# seleciona conjunto de contrados com licitacoes do modelo "Dispensa de valor" ou "Dispensa por outro motivo"
+nu_Dispensas <- subset(contrato, tp_Licitacao %in% c(6, 7)) %>% 
+  group_by(cd_UGestora, dt_Ano) %>% summarise(nu_Dispensas = length(tp_Licitacao))
 tre_sagres <- merge(tre_sagres, nu_Dispensas, all.x = T, by.x=c("cd_Ugestora","dt_Ano"), by.y = c("cd_UGestora","dt_Ano"))
 tre_sagres$nu_Dispensas <- with(tre_sagres, ifelse(is.na(nu_Dispensas),0,nu_Dispensas))
-
-# Aplica a funcao "relabel_ano" ao conjunto "Aditivos"
-aditivos$dt_Ano <- with(aditivos, unlist(lapply(dt_Ano, relabel_ano)))
 
 # Adiciona os atributos de aditivos
 aditivo_De_Prazo <- filter(aditivos, vl_Aditivo == "0,0000")
@@ -58,15 +66,10 @@ tre_sagres[is.na(tre_sagres)] <- 0
 # Adiciona convite de Licita??es
 ## Seleciona todos os contratos do tipo convite
 conviteLicitacaoPorGestao <- filter(contrato, tp_Licitacao == 3)
-conviteLicitacaoPorGestao$dt_Ano <- with(conviteLicitacaoPorGestao, unlist(lapply(dt_Ano, relabel_ano)))
+#conviteLicitacaoPorGestao$dt_Ano <- with(conviteLicitacaoPorGestao, unlist(lapply(dt_Ano, relabel_ano)))
 conviteLicitacaoPorGestao <- aggregate(nu_Contrato ~ cd_UGestora + dt_Ano, conviteLicitacaoPorGestao, length)
 colnames(conviteLicitacaoPorGestao)[3] <- "nu_Convites"
 tre_sagres <- merge(tre_sagres, conviteLicitacaoPorGestao, by.x = c("cd_Ugestora","dt_Ano"), by.y = c("cd_UGestora","dt_Ano"), all.x = T)
-
-# Adiciona Quantidade de Eleitores por Municipio e Distancia da capital
-quantidadeEleitores = select(quantidadeEleitores, Abrangencia, Quantidade2009, Quantidade2013, DistanciaParaCapital)
-quantidadeEleitores <- group_by(quantidadeEleitores, Abrangencia) %>% mutate(Media = (Quantidade2009 + Quantidade2013)/2)
-tre_sagres <- merge(tre_sagres, quantidadeEleitores, by.x = c("de_Ugestora"), by.y = c("Abrangencia"), all.x = T)
 
 tre_sagres[is.na(tre_sagres)] <- 0
 tre_sagres <- unique(tre_sagres)
